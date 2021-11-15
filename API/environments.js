507e8a429dc520242028a5c3ac01784fb84beda2
adrianosilva1981 <47525975+adrianosilva1981@users.noreply.github.com>
2021-11-15 10:05:04 -0300

    Update environments.js

--- API/environments.js
+++ API/environments.js
@@ -1,4 +1,4 @@
-const _db_connection = 'mongodb://usr_sesi:sesi0302@127.0.0.1:27017/db_sesi';
+const _db_connection = 'mongodb://user:pass@127.0.0.1:27017/db';
 const _production = false;
 
 exports.db_connection = _db_connection;
